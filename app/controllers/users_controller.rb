c54c96bfcf993fb5ff19d13b5c1651521e21484c
PandaGrom <arsenij.grom@mail.ru>
2022-01-08 02:14:52 +0300

    add ability to block users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new
   end
+
+  def block
+    user = User.find(params[:id])
+    user.blocked = true
+    user.save
+    redirect_to user_path(user)
+  end
+
+  def unblock
+    user = User.find(params[:id])
+    user.blocked = false
+    user.save
+    redirect_to user_path(user)
+  end
 end
