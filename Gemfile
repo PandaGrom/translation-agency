8c0c089fe87d87bc144bc1625f56e47fc05ebb54
PandaGrom <arsenij.grom@mail.ru>
2022-01-10 00:57:08 +0300

    I18n

--- Gemfile
+++ Gemfile
@@ -36,7 +36,7 @@ gem 'bootstrap', '~> 5.1.3'
 gem 'htmlbeautifier'
 gem 'jquery-rails'
 gem 'sidekiq'
-
+gem 'rails-i18n'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   include Pundit
+
+  around_action :switch_locale
+
+  private
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale locale, &action
+  end
+  
+  def locale_from_url
+    locale = params[:locale]
+
+    return locale if I18n.available_locale.map(&:to_s).include?(locale)
+  end
+  
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+  
 end
